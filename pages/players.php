29946a43eb650af7fc4d9ed2f27c09a9ecc9fae9
arnav1640 <arnavgaddam01@gmail.com>
2021-12-10 20:36:53 -0800

    formatted player with db info

--- pages/players.php
+++ pages/players.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" style="background-image: url(../assets/whitebackground.jpg);">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../styles/players.css">
+    <link rel="stylesheet" type="text/css" href="../styles/styles.css">
     <title>Players</title>
 </head>
 <body>
-    <ul class="navbar">
+    <ul>
         <li><a href="about.asp">🔎</a></li>
         <li><a href="../pages/index.html">Home</a></li>
         <li><a href="players.php">Players</a></li>
@@ -33,16 +33,32 @@
 
 ?>
 
-        <ul>
-            <li class = "label">Player Name     PPG </li>
-        <?php  
-        if($result -> num_rows> 0){
-            while($row = $result->fetch_assoc()) { ?>
-            <li class = "player_data"> <?php echo  $row['name'] . "      " .  $row['PPG']; ?> </li>
+        <table style="height:50vh;width:97vw; position: relative; top: 0; bottom: 0; left: 0; right: 0;border:6px double black; border-radius: 10px">
+            <tr style="font-size: 25px; text-align: left; outline: 3px groove; font-family: Trebuchet MS">
+                <th>Player Name</th>
+                <th>Team</th>
+                <th>Pos</th>
+                <th>PPG</th>
+                <th>2P%</th>
+                <th>3P%</th>
 
-            <?php }} ?>
+            </tr>
+            <?php 
+                if($result -> num_rows > 0){
+                    while($row = $result->fetch_assoc()) { ?>
+                    <tr class="data" style="outline: 1px dashed gray; font-family: Trebuchet MS">
+                        <td><?php echo $row['name'];?></td>
+                        <td><?php echo $row['team'];?></td>
+                        <td><?php echo $row['position'];?></td>
+                        <td><?php echo $row['PPG'];?></td>
+                        <td><?php echo $row['2P%'];?></td>
+                        <td><?php echo $row['3P%'];?></td>
+                    </tr>
+                 <?php   }
+                } ?>
+        </table>
+        
 
-    </ul>
 
           
 </body>
